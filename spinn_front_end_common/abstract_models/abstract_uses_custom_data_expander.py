4de4b2e9ba66af7f17ee2994c102138726a56f57
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-07-05 16:33:08 +0100

    Adding the algorithm for PDE

--- spinn_front_end_common/abstract_models/abstract_uses_custom_data_expander.py
+++ spinn_front_end_common/abstract_models/abstract_uses_custom_data_expander.py
@@ -9,20 +9,21 @@ class AbstractUsesCustomDataExpander(object):
         data and needs to be executed before the vertex executable itself.
         Note that all data expanders will be executed synchronously.
     """
-    
+
     @abstractmethod
     def get_data_expander_executable(self):
         """ Get the name of the executable used to expand the data.
             The expander is expected to be found with an ExecutableFinder
-            
+
         :return: The name of an executable
         :rtype: str
         """
-    
+
     @abstractmethod
     def write_data_expander_data(self, file_writer):
         """ Write the data required by the expander
-        
+
         :param file_writer: The file to write the data to
-        :type file_writer: `py:class:spinn_storage_handlers.FileW`
+        :type file_writer:\
+            `py:class:spinn_storage_handlers.abstract_classes.AbstractDataWriter`
         """
---
+++ spinn_front_end_common/interface/interface_functions/front_end_common_execute_custom_data_expander.py
@@ -0,0 +1,8 @@
+
+class FrontEndCommonExecuteCustomDataExpander(object):
+    """ An algorithm for executing custom data expanders in the graph\
+        on the machine
+    """
+
+    def __call__(self, graph):
+        pass
